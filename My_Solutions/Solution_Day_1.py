043021d244e35155e0941082f92cf7efdbdc120e
BaapOfCoding699 <gautamgaurav952@gmail.com>
2026-04-16 19:27:42 +0530

    Day 1 finished

---
+++ My_Solutions/Solution_Day_1.py
@@ -0,0 +1,54 @@
+#  Leap year
+
+# x = int(input("Enter the year : "))
+# if(x % 4 == 0) and (x % 100 != 0 or x % 400 == 0):
+#     print(f"The given year {x} is a leap year.")
+# else:
+#     print("The given year is not a leap year.")
+
+# Sum of 2 numbers
+
+# y = int(input("Enter number 1 : "))
+# z = int(input("Enter number 2 : "))
+# a = y + z
+# print(f"Sum of 2 number is {a}")
+
+# Multiplication Table
+
+# b = int(input("Enter a number : "))
+# i = 1
+# while ( i <= 10):
+#     c = b * i
+#     print(f"{b} * {i} = {c}")
+#     i = i + 1
+
+# HCF and LCM
+
+# a = int(input("Enter first number : "))
+# b = int(input("Enter second number : "))
+
+# temp1 = a
+# temp2 = b
+
+# while b != 0:
+#     rem = a % b
+#     a = b
+#     b = rem
+
+# hcf = a
+# lcm = (temp1 * temp2 ) / hcf
+
+# print(f"The hcf is {hcf}")
+# print(f"The lcm is {lcm}")
+
+
+# sum until x
+
+# value = 0
+# while True:
+#     num = input("Enter the value : ")
+#     if num == "x":
+#         break
+#     else:
+#         value = int(num) + value
+# print(f"The sum is : {value}")
