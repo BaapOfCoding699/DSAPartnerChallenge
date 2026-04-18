3f9db42e680ac51f2604723634cf59b2a2aa78ec
BaapOfCoding699 <gautugaurav@gmail.com>
2026-04-18 20:10:20 +0530

    Day 3 finished

---
+++ My_Solutions/Solution_Day_3.py
@@ -0,0 +1,35 @@
+import math
+
+# Area of Circle
+
+# x = int(input("Enter the radius of circle : "))
+# y = round(math.pi * x**2,2)
+# print("The area of circle is : ",y)
+
+# ----------------------------------------------------------------------
+
+# Area of Triangle
+
+# x = int(input("Enter the base : "))
+# y = int(input("Enter the height : "))
+
+# z = round(((1/2) * x * y),2)
+# print(f"The area of triangle is : {z}")
+
+# ------------------------------------------------------
+
+# Area of Rhombus
+
+# x = int(input("Enter the first daigonal : "))
+# y = int(input("Enter second daigonal : "))
+# z = (1/2) * y * x
+# print("The area of rhombus is : ", z)
+
+# -------------------------------------------------
+
+# Perimeter of Parallelogram
+
+# x = int(input("Enter the first adjacent side : "))
+# y = int(input("Enter the second adjacent side : "))
+# z = 2 * (x + y )
+# print(f"The perimeter of parallelogram is : {z}")
